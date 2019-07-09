17011426e3dfa43276fc2201667f4cb004db3947
Mae Rine <mae.s.rine@gmail.com>
2019-07-09 20:13:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,8 +66,9 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
+    var order = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return order;
   }
 }
 
