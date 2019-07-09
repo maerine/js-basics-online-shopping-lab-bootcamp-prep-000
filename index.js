542244886e37ab26fe62c8c90265de98620466bc
Mae Rine <mae.s.rine@gmail.com>
2019-07-09 20:10:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    
+    cart = [];
+    return ``
   }
 }
 
